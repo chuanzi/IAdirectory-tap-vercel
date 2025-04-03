e8df066eb221e229d247134a1a746edc74599ced
chuanzi <tianjiangchuanzi@163.com>
2025-04-03 15:07:32 +0800

    vercel 部署问题修改

--- db/supabase/client.ts
+++ db/supabase/client.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 
-import { cookies } from 'next/headers';
-import { createBrowserClient, createServerClient } from '@supabase/ssr';
+import { createBrowserClient } from '@supabase/ssr';
 
 import { Database } from './types';
 
@@ -12,9 +11,16 @@ export function createClient() {
   );
 }
 
-export function createServerComponentClient() {
+// 注意：此下方函数仅用于App Router，不能在Pages Router中使用
+// This function can only be used in App Router, not in Pages Router
+// 不要在Pages目录的组件中导入此函数
+// eslint-disable-next-line
+export async function createServerComponentClient() {
+  // 动态导入，这样在构建时不会被包含到Pages路由的客户端代码中
+  const { cookies } = await import('next/headers');
   const cookieStore = cookies();
 
+  const { createServerClient } = await import('@supabase/ssr');
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
